b034d6a6ccc99cf456e139aac625da58de0b6da0
Tahsin Alahi <alahitahsin@gmail.com>
2026-02-01 06:12:06 +0600

    refactor: IdGenerator with in-depth study

--- src/utils/IdGenerator.java
+++ src/utils/IdGenerator.java
@@ -1,9 +1,29 @@
 package utils;
 
-public class IdGenerator {
-    private static int id = 1;
+import java.util.concurrent.atomic.AtomicInteger;
 
-    public static int generateId(){
-        return id++;
+public class IdGenerator {
+    /**
+     * AtomicInteger uses CAS (Compare-And-Swap) internally to achieve lock-free thread safety.
+     * And as it achieves thread safety without lock it is a bit more light weight than synchronized.
+     */
+    private static final AtomicInteger id = new AtomicInteger(1);
+
+    /**
+     * This is a utility class there's no need to make an object of it
+     */
+    private IdGenerator() {
+        throw new AssertionError("Utility Class");
     }
+
+    /**
+     * synchronized could be used here but when the race condition happen only one task gets executed also it uses lock.
+     * But the other tasks are either suspended or blocked.
+     * And it's expensive to resume a suspending task.
+     */
+    public static int generateId() {
+        return id.incrementAndGet();
+    }
+
+
 }
